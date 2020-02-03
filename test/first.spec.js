bcb92ef0bdd1a209666dfdeb640168dac2665a93
Carlo Roberto <crgmzlps@gmail.com>
2020-02-03 15:03:16 -0300

    Initial setup

---
+++ test/first.spec.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("A feature", function() {
+  describe("A scenario", function() {
+    it("does something expected", function() {
+      assert(true);
+    });
+  });
+});
